5c1b76fd4305fa8a67cd6c3d42a2c79a64d337e4
Syed A Hasan <[absaarcv@outlook.com]>
2016-04-18 09:02:43 +0100

    success service fix

--- app/views/success/service.js
+++ app/views/success/service.js
@@ -13,28 +13,21 @@ function successService() {
 
     function activate($stateParams) {
 
-        console.log($stateParams.event)
-        console.log('A')
-
         if ($stateParams.event === 'add') {
 
             service.msg.response = "Your new mammal has been added!"
-            console.log('B')
 
         } else if ($stateParams.event === 'edit') {
 
             service.msg.response = "Your changes are now live!"
-            console.log('C')
 
         } else if ($stateParams.event === 'delete') {
 
             service.msg.response = "This mammal has now been deleted!"
-            console.log('D')
 
         } else {
 
             service.msg.response = "Oops, you appear to have landed here by mistake ..."
-            console.log('E')
 
         }
     }
